aa9a7b47eb760361f4663dfa1454fdadfc4075c4
Ryan Brunner <ryan@influitive.com>
2014-03-13 21:23:11 -0400

    Todo list working

--- todo.rb
+++ todo.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@todos = []
+@@todos = ["Mow the lawn", "Walk the dog"]
 
 get '/' do 
   erb :todos, locals: {
-    todos: @@todos  
+    todo_list: @@todos  
   }
+end
+
+# http://localhost:4567/add?todo=Mow the lawn
+post '/add' do
+  @@todos << params[:todo]
+  redirect to('/')
+end
+
+post '/remove/:i' do
+  @@todos.delete_at(params[:i].to_i)
+  redirect to('/')
 end
